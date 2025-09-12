92ee1f9991e65530bfb7c62f8d87ac36eeb9c0d4
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-12 14:13:46 +0330

    Main City Structure

--- src/components/MainCity/MainCity.jsx
+++ src/components/MainCity/MainCity.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { PiWind } from "react-icons/pi";
 import { PiDropLight } from "react-icons/pi";
 import { PiEye } from "react-icons/pi";
@@ -6,25 +6,27 @@ import { PiEye } from "react-icons/pi";
 import MainCityInfoBox from './MainCityInfoBox/MainCityInfoBox';
 
 
+
 export default function MainCity() {
+
   return (
     <>
-        <div className='rounded-2xl flex flex-col items-center justify-between gap-y-8 bg-gradient-to-t from-blue-600 via-blue-400 to-blue-300 w-80 h-80 p-5 text-gray-100'>
-            <div className='flex flex-col mr-auto'>
+        <div className='rounded-3xl flex flex-col items-center justify-between bg-gradient-to-t from-blue-600 via-blue-400 to-blue-300 p-5 text-gray-100 overflow-hidden h-full'>
+            <div className='flex flex-col mr-auto font-NunitoLight leading-5'>
               <span>Today, 02 jun 2025</span>
               <span>Sydney</span>
             </div>
-            <div className='flex flex-col'>
-              <span>Mostly Clear</span>
-              <span>24</span>
-              <span>icon</span>
+            <div className='flex flex-col items-center'>
+              <span className='font-NunitoLight'>Mostly Clear</span>
+              <span className='text-[115px] leading-[0.8] font-NunitoSemibold text-transparent bg-gradient-to-b from-white via-white/70 to-white/5 bg-clip-text'>24</span>
+              <img src='/images/weather-status/mostly_clear.png' alt="" className='w-40 h-40 -mt-12'/>
             </div>
-            <div className='flex items-center w-full justify-between'>
+            <div className='flex items-center w-full justify-around'>
               <MainCityInfoBox title='Wind' data='9 km / h' icon={<PiWind className='w-8 h-8'/>}/>
               <span className='w-[1px] h-11 bg-white/60 rounded-full'></span>
               <MainCityInfoBox title='Humidity' data='25 %' icon={<PiDropLight className='w-8 h-8'/>}/>
               <span className='w-[1px] h-11 bg-white/60 rounded-full'></span>
-              <MainCityInfoBox title='Visibility' data='1.7 km' icon={<PiEye className='w-8 h-8'/>}/>
+              <MainCityInfoBox title='Visibility' data='17 km' icon={<PiEye className='w-8 h-8'/>}/>
             </div>
         </div>
     </>
