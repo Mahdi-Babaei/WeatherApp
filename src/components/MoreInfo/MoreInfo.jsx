aea17e87c60bf387c097bb5377980fdcd283de43
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-17 13:46:09 +0330

    AQ Structure

--- src/components/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfo/MoreInfo.jsx
@@ -31,14 +31,20 @@ export default function MoreInfo() {
                   <span className="font-NunitoSemibold">Moonset: 00:00</span>
               </div>
           </div>
-          <div className="grid grid-cols-2 text-center gap-x-4">
+          <div className="grid grid-cols-2 text-center gap-x-6">
+
             <div className="flex flex-col gap-y-3">
               <h3 className="font-NunitoSemibold">Air Quality</h3>
-              <div className="relative w-full bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 h-3 rounded-full">
+              <div className="relative w-full bg-orange-300/70 h-3 rounded-full">
+                <span className="h-3 rounded-full bg-orange-500 block absolute top-0 w-[30%]"></span>
                 <span className="w-6 h-6 rounded-full bg-orange-500 border-2 block absolute -top-1.5 left-1/4"></span>
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, consectetur!</p>
+              <div>
+                <h6>Unhealthy</h6>
+                <p className="text-base h-18">Limit strenuous outdoor activities, especially if you have a health condition.</p>
+              </div>
             </div>
+
             <div className="flex flex-col gap-y-3">
               <h3 className="font-NunitoSemibold">UV Index</h3>
               <div className="relative w-full h-3 rounded-full" style={{background: 'linear-gradient(90deg,rgba(65, 176, 0, 1) 0%, rgba(250, 204, 0, 1) 25%, rgba(227, 106, 0, 1) 50%, rgba(255, 0, 0, 1) 75%, rgba(128, 0, 219, 1) 100%)'}}>
@@ -46,7 +52,7 @@ export default function MoreInfo() {
               </div>
               <div>
                 <h6>High</h6>
-                <p className="text-base">Reduce time in the sun, especially during midday. Take all precautions. </p>
+                <p className="text-base h-18">Reduce time in the sun, especially during midday. Take all precautions. </p>
               </div>
             </div>
           </div>
