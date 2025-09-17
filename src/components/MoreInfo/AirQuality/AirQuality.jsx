82566fd0a1fccd3181afbd93da176b44720e28d6
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-17 15:38:02 +0330

    AQ Slider Bg color

--- src/components/MoreInfo/AirQuality/AirQuality.jsx
+++ src/components/MoreInfo/AirQuality/AirQuality.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetchData from '../../../hooks/useFetchData'
 
-export default function AirQuality() {
+const AQconditions = [
+  {title: 'Good' , desc: 'The air is clean and fresh. Perfect for outdoor fun!' , epaIndex : 1 , bgColor: 'bg-green-300/70' , color: 'bg-green-500'} ,
+  {title: 'Moderate' , desc: 'Sensitive groups might consider taking it easy outdoors' , epaIndex : 2 , bgColor: 'bg-yellow-300/70' , color: 'bg-yellow-500'} ,
+  {title: 'Unhealthy for Sensitive Groups' , desc: 'Limit strenuous outdoor activities, especially if you have a health condition.' , epaIndex : 3 , bgColor: 'bg-yellow-300/70' , color: 'bg-yellow-500'} ,
+  {title: 'Unhealthy' , desc: 'Unhealthy air for everyone. Avoid prolonged time outdoors.' , epaIndex : 4 , bgColor: 'bg-orange-300/70' , color: 'bg-orange-500'} ,
+  {title: 'Very Unhealthy' , desc: 'The air is a health risk. Stay indoors and avoid all outdoor activity.' , epaIndex : 5 , bgColor: 'bg-orange-300/70' , color: 'bg-orange-500'} ,
+  {title: 'Dangerous' , desc: 'Emergency level air quality. A life-threatening risk. Remain indoors and do not go outside.' , epaIndex : 6 , bgColor: 'bg-red-300/70' , color: 'bg-red-500'} ,
+]
+
+export default function AirQuality({city}) {
+  const {fetchData , isLoading} = useFetchData('current' , city , undefined , 'yes')
+
+
+
+  const sliderColor = (aq) => {
+    let currentAirObj = AQconditions.filter(item => {
+        return item.epaIndex === 6
+    })[0]
+    return currentAirObj
+  }
+  
   return (
     <>
-      <div className="flex flex-col gap-y-3">
-        <h3 className="font-NunitoSemibold">Air Quality</h3>
-        <div className="relative w-full bg-orange-300/70 h-3 rounded-full">
-          <span className="h-3 rounded-full bg-orange-500 block absolute top-0 w-[30%]"></span>
-          <span className="w-6 h-6 rounded-full bg-orange-500 border-2 block absolute -top-1.5 left-1/4"></span>
+      {isLoading ? <p>Loading....</p> : !fetchData ? null : (
+        <div className="flex flex-col gap-y-3">
+          <h3 className="font-NunitoSemibold">Air Quality</h3>
+          <div className={`relative w-full h-3 rounded-full ${sliderColor(fetchData.current.air_quality['us-epa-index']).bgColor}`}>
+            <span className={`h-3 rounded-full block absolute top-0 w-[30%] ${sliderColor(fetchData.current.air_quality['us-epa-index']).color}`}></span>
+            <span className={`w-6 h-6 rounded-full border-2 block absolute -top-1.5 left-1/4 ${sliderColor(fetchData.current.air_quality['us-epa-index']).color}`}></span>
+          </div>
+          <div>
+            <h6>{AQconditions.filter(item => item.epaIndex === fetchData.current.air_quality['us-epa-index'])[0].title}</h6>
+            <p className="text-base h-18">{AQconditions.filter(item => item.epaIndex === fetchData.current.air_quality['us-epa-index'])[0].desc}</p>
+          </div>
         </div>
-        <div>
-          <h6>Unhealthy</h6>
-          <p className="text-base h-18">
-            Limit strenuous outdoor activities, especially if you have a health
-            condition.
-          </p>
-        </div>
-      </div>
+      )}
     </>
   );
 }
--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -3,12 +3,11 @@ import React, { useEffect, useState } from 'react'
 const weatherApi = 'https://api.weatherapi.com/v1'
 const apiKey = '34ed6d8bb44d46aebf980813251109'
 
-export default function useFetchData(apiMethod , targetCity , days = '1') {
+export default function useFetchData(apiMethod , targetCity , days = '1' , aqi = 'no') {
     const [fetchData , setFetchData] = useState(null)
     const [isLoading , setIsLoading] = useState(true)
-    const url = `${weatherApi}/${apiMethod}.json?key=${apiKey}&q=${targetCity}&days=${days}`
-
-
+    const url = `${weatherApi}/${apiMethod}.json?key=${apiKey}&q=${targetCity}&days=${days}&aqi=${aqi}`
+    
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
