12c439bcfed5de16c606690d03a49f1850957306
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-19 16:09:44 +0330

    Forecast Light Theme

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -13,12 +13,12 @@ export default function Forecast({city}) {
   return (
     <>
         {isLoading ? <p>Loading ...</p> : !fetchData ? null : (
-            <div className='bg-dark-secondary rounded-3xl flex flex-col justify-between gap-y-5 py-5 px-8 h-full relative'>
+            <div className='bg-light-secondary dark:bg-dark-secondary rounded-3xl flex flex-col justify-between gap-y-5 py-5 px-8 h-full relative'>
                 <div className='flex justify-between items-center'>
                     <h2 className='font-NunitoBold text-3xl'>Next 7 Days</h2>
                     <span className='text-blue-400 font-NunitoSemibold text-lg cursor-pointer hover:text-blue-500 transition-all'>See All</span>
                 </div>
-                <div className='bg-gradient-to-t from-blue-500 via-blue-400  to-blue-300 -mx-5 px-5 py-5 rounded-3xl'>
+                <div className='bg-gradient-to-t from-blue-500 via-blue-400  to-blue-300 -mx-5 px-5 py-5 rounded-3xl text-zinc-100'>
                     {fetchData.forecast.forecastday.slice(1 , 2).map(item => {
                         return (
                             <div className='grid grid-cols-3 text-xl  font-NunitoLight items-center'>
