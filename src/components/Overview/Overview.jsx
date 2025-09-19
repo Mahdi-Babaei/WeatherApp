032741a80a31080daebf952c72caee9d5e746c14
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-19 16:15:44 +0330

    Overview Light Theme

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -5,13 +5,13 @@ export default function Overview() {
     const [activeChart , setActiveChart] = useState('Temperature')
   return (
     <>
-        <div className='bg-dark-secondary p-5 rounded-3xl flex flex-col justify-between h-full'>
+        <div className='bg-light-secondary dark:bg-dark-secondary p-5 rounded-3xl flex flex-col justify-between h-full'>
             <div className='flex items-center justify-between'>
                 <h2 className='font-NunitoBold text-3xl'>Overview</h2>
-                <div className='flex items-center justify-center rounded-3xl border border-white/20 p-1 text-gray-500/40'>
+                <div className='flex items-center justify-center rounded-3xl border border-gray-300 dark:border-white/20 p-1 text-gray-500/40'>
                     {['Temperature' , 'Humidity' , 'Rainfall'].map(item => {
                         return (
-                            <h3 className={`py-2 px-4 cursor-pointer transition-all ${activeChart === item ? 'text-gray-100 bg-zinc-800 rounded-3xl' : 'text-gray-500/40 hover:text-zinc-100 cursor-pointer'}`} onClick={() => setActiveChart(item)}>{item}</h3>
+                            <h3 className={`py-2 px-4 cursor-pointer transition-all ${activeChart === item ? 'text-black dark:text-zinc-100 bg-light-primary dark:bg-zinc-800 rounded-3xl ' : 'text-gray-500/40 hover:text-black dark:hover:text-zinc-100 cursor-pointer'}`} onClick={() => setActiveChart(item)}>{item}</h3>
                         )
                     })}
                 </div>
