873168b887fc63f5d9cdf3948aaad6e1f56f485f
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-19 16:10:58 +0330

    More Info Light Theme

--- src/components/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfo/MoreInfo.jsx
@@ -7,7 +7,7 @@ import UV from "./UV/UV";
 export default function MoreInfo({city}) {
   return (
     <>
-      <div className="bg-dark-secondary rounded-3xl flex flex-col gap-y-8 py-5 px-8 h-full text-xl font-NunitoLight">
+      <div className="bg-light-secondary dark:bg-dark-secondary rounded-3xl flex flex-col gap-y-8 py-5 px-8 h-full text-xl font-NunitoLight">
           <h2 className="font-NunitoBold text-3xl">More Info</h2>
           <Astro city={city}/>
           <div className="grid grid-cols-2 text-center gap-x-6">
