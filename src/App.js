cdd2bf0330903ffba65ead0c0cb0ecf2816b7a8f
Mahdi-babaei <mahdibabaei.8626@gmail.com>
2025-09-21 12:02:46 +0330

    react responsive package installed

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { ThemeContext } from './context/Theme';
 import { CityContext } from './context/City';
 import Desktop from './breakpoints/Desktop';
 import Mobile from './breakpoints/Mobile';
+import { useMediaQuery } from 'react-responsive'
+
 
 export default function App() {
   const {theme , setTheme} = useContext(ThemeContext)
   const {city , setCity , favCities} = useContext(CityContext)
-
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1280px)'
+  })
   
 
   return (
     <>
       <div className={`h-full 2xl:h-screen bg-light-primary dark:bg-dark-primary ${theme} p-4 2xl:p-10 flex gap-x-5 select-none dark:text-gray-100 `}>
-          {/* <Mobile city={city} favCities={favCities} /> */}
-          <Desktop city={city} favCities={favCities}/>
+        {isDesktop ? <Desktop city={city} favCities={favCities}/> : <Mobile city={city} favCities={favCities}/>}
       </div>
     </>
   );
